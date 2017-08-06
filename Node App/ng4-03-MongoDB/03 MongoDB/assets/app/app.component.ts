e9f03c7fbc77959ca488409597360c2f5fa392cd
Shashank Shekhar <shashankshekhars19@gmail.com>
2017-08-07 00:31:45 +0530

    Second Commit

--- Node App/ng4-03-MongoDB/03 MongoDB/assets/app/app.component.ts
+++ Node App/ng4-03-MongoDB/03 MongoDB/assets/app/app.component.ts
@@ -1,30 +1,11 @@
 import { Component } from '@angular/core';
+import { Message } from './message/message.model';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html',
-    styles : [`
-        .author {
-            display  : inline-block;
-            font-style : italic;
-            font-size : 12px;
-            width : 80%;
-       }
-       .config {
-           display : inline-block;
-           text-align : right;
-           font-size : 12px;
-           width : 19%
-       }
-    
-    
-    
-    
-    `]
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-    message = {
-        content : 'A message',
-        author : 'Max'
-    };
+    message : Message  = new Message ( 'A message', 'Max');
+
 }
